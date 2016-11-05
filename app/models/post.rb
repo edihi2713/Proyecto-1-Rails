fcdbcad1346b67f7cc4a80643460bc08cb5d6fd0
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-11-05 16:35:45 -0500

    Hasta comentarios

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+end
